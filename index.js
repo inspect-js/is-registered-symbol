27b443f65db1ef16107d5ae17087e96bdc447548
Jordan Harband <ljharb@gmail.com>
2024-12-15 23:40:21 -0800

    [Refactor] use `call-bound` directly

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var isSymbol = require('is-symbol');
-var callBound = require('call-bind/callBound');
+var callBound = require('call-bound');
 
-/** @type {typeof Symbol.keyFor} */
+/** @type {undefined | typeof Symbol.keyFor} */
 var keyFor = callBound('Symbol.keyFor', true);
-/** @type {(receiver: ThisParameterType<typeof Symbol.prototype.valueOf>, ...args: Parameters<typeof Symbol.prototype.valueOf>) => ReturnType<typeof Symbol.prototype.valueOf>} */
+/** @type {undefined | ((receiver: ThisParameterType<typeof Symbol.prototype.valueOf>, ...args: Parameters<typeof Symbol.prototype.valueOf>) => ReturnType<typeof Symbol.prototype.valueOf>)} */
 var symbolValueOf = callBound('Symbol.prototype.valueOf', true);
 
 /** @type {import('.')} */
@@ -13,5 +13,9 @@ module.exports = function isRegisteredSymbol(sym) {
 	if (!isSymbol(sym)) {
 		return false;
 	}
-	return typeof keyFor(typeof sym === 'symbol' ? sym : symbolValueOf(sym)) === 'string';
+	// eslint-disable-next-line no-extra-parens
+	return typeof /** @type {NonNullable<typeof keyFor>} */ (keyFor)(typeof sym === 'symbol'
+		? sym
+		// eslint-disable-next-line no-extra-parens
+		: /** @type {NonNullable<typeof symbolValueOf>} */ (symbolValueOf)(sym)) === 'string';
 };
